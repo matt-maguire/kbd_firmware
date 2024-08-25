9f2dfa4c5707be383f73bde69c9c1e772a4f9133
Matt Maguire <matthew.maguire@gmail.com>
2024-08-25 15:55:37 +1000

    update mini keymap with ISRT & CAPS_WORD

---
+++ keyboards/crkbd/vial-kb/vial-qmk/keymaps/vial_mini_custom/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x89, 0x36, 0x2A, 0xC7, 0xFA, 0xD8, 0x89, 0x45}
+//#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_ROWS {4, 4}
+#define VIAL_UNLOCK_COMBO_COLS {1, 2}
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
